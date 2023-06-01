1b65f2094db05cfd4c8af07703658329ef09834e
Nomer Hapa <nomerhapa01@gmail.com>
2023-06-01 15:34:07 +0800

    .

---
+++ src/App.js
@@ -0,0 +1,23 @@
+import "./App.css";
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+import { Home } from "./screens/Home";
+import { Tutorial } from "./screens/Tutorial";
+import { ErrorMessage } from "./components/ErrorMessage";
+import { Search } from "./screens/Search";
+
+function App() {
+  return (
+    <>
+      <BrowserRouter>
+        <Routes>
+          <Route path="/" element={<Home />} />
+          <Route path="tutorial/:id" element={<Tutorial />} />
+          <Route path="*" element={<ErrorMessage />} />
+          <Route path="/search/:queryTerm" element={<Search />} />
+        </Routes>
+      </BrowserRouter>
+    </>
+  );
+}
+
+export default App;
---
+++ src/hooks/useFetch.js
@@ -0,0 +1,35 @@
+import { useState, useEffect } from "react";
+
+const useFetch = (queryTerm) => {
+  const [recipes, setRecipes] = useState([]);
+  const [loading, setLoading] = useState(true);
+  const [error, setError] = useState(null);
+
+  console.log(queryTerm);
+
+  useEffect(() => {
+    const fetchData = async () => {
+      try {
+        const response = await fetch(
+          `https://api.edamam.com/api/recipes/v2?type=public&q=${queryTerm}&app_id=ef00f4db&app_key=ba56c90bd2895b66f54d802183ec00b1`
+        );
+        if (!response.ok) {
+          throw new Error("Failed to fetch recipes");
+        }
+        const data = await response.json();
+        setRecipes(data.hits);
+        // setRecipes(data);
+        setLoading(false);
+      } catch (error) {
+        setError(error);
+        setLoading(false);
+      }
+    };
+
+    fetchData();
+  }, []);
+
+  return { recipes, loading, error };
+};
+
+export default useFetch;
